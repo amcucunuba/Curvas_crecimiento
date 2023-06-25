2e328ac9b72fc2ad6f447feab82da27fca20986a
Angela Maria <amcucunubar@gmail.com>
2023-06-25 18:47:16 +0200

    Limpieza y orden en las hojas del proyecto

---
+++ Main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import math
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from Funcion import analis_edad
+from Funcion import edad_meses
+
+
+
+#Datos que el usuario debe ingresar
+nombre = input("Ingrese el nombre de su bebé:  ")
+fecha_ingresada = input("Ingrese la fecha de nacimiento (DD/MM/AAAA): ")
+genero_ingresado = input("Ingrese el género, Femenino  F o Masculino M:  ")
+talla_ingresada = float(input("Ingrese la talla en cm:  "))
+peso_ingresado = float(input("Ingrese el peso en kg:  "))
+
+
+analis_edad(genero_ingresado, edad_meses(fecha_ingresada), talla_ingresada, peso_ingresado, nombre)    
